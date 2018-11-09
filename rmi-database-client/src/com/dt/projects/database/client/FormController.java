fde88d432440297b27b068d530aa493502439b03
kastu <sharma.kasturi250@gmail.com>
2018-11-09 23:43:21 +0530

    database successfull connection with xampp

---
+++ rmi-database-client/src/com/dt/projects/database/client/FormController.java
@@ -0,0 +1,238 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dt.projects.database.client;
+
+import com.dt.projects.database.api.entity.Menu;
+import com.dt.projects.database.api.services.MenuService;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+
+/**
+ * FXML Controller class
+ *
+ * @author kastu
+ */
+public class FormController implements Initializable {
+
+    @FXML
+    private TextField txtname;
+    @FXML
+    private TextField txtid;
+    @FXML
+    private TextField txtprice;
+    @FXML
+    private TextField txttype;
+    @FXML
+    private TableView<Menu> tableView;
+    @FXML
+    private TableColumn<Menu, Long> colId;
+    @FXML
+    private TableColumn<Menu, String> colName;
+    @FXML
+    private TableColumn<Menu, String> colType;
+    @FXML
+    private TableColumn<Menu, Integer> colRate;
+    
+    private Main main;
+    
+    private MenuService menuService;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        colId.setCellValueFactory(new PropertyValueFactory<>("item_id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("item_name"));
+        colType.setCellValueFactory(new PropertyValueFactory<>("item_type"));
+        colRate.setCellValueFactory(new PropertyValueFactory<>("item_price"));
+        
+        tableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Menu>(){
+            @Override
+            public void changed(ObservableValue<? extends Menu> ov, Menu oldMenu, Menu newMenu) {
+                
+                if(newMenu != null)
+                {
+                    txtid.setText(Long.toString(newMenu.getItem_id()));
+                    txtname.setText(newMenu.getItem_name());
+                    txttype.setText(newMenu.getItem_type());
+                    txtprice.setText(Integer.toString(newMenu.getItem_price())); 
+                }
+                else
+                {
+                    clearFields();
+                }
+            }
+            
+    });
+    }    
+
+    @FXML
+    private void onInsert(ActionEvent event) {
+        
+        if(isFieldValid())
+        {
+           try
+            {
+                Menu menu = new Menu();
+                menu.setItem_name(txtname.getText());
+                menu.setItem_type(txttype.getText());
+                menu.setItem_price(Integer.parseInt(txtprice.getText()));
+
+                menu= menuService.insertMenu(menu);
+
+                tableView.getItems().add(menu);
+
+                clearFields();
+            }
+            catch(RemoteException ex)
+            {
+                ex.printStackTrace();
+            }  
+        }
+        
+    }
+
+    @FXML
+    private void onUpdate(ActionEvent event) {
+        int index = tableView.getSelectionModel().getSelectedIndex();
+        
+        if(index == -1)
+        {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.initModality(Modality.APPLICATION_MODAL);
+            alert.setTitle("Update");
+            alert.setHeaderText("No item selected");
+            alert.setContentText("Please select an item in the table");
+            alert.showAndWait();
+            return;
+        }
+        
+        if(isFieldValid())
+        {
+            try
+            {
+                Menu menu = new Menu();
+                menu.setItem_id(Long.valueOf(txtid.getText()));
+                menu.setItem_name(txtname.getText());
+                menu.setItem_type(txttype.getText());
+                menu.setItem_price(Integer.parseInt(txtprice.getText()));
+                
+                menuService.updateMenu(menu);
+                
+                tableView.getItems().set(index, menu);
+                
+                clearFields();
+                
+            }catch(RemoteException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    private void onDelete(ActionEvent event) {
+        
+        try
+        {
+            Menu menu = tableView.getSelectionModel().getSelectedItem();
+            if(menu == null)
+            {
+                return;
+            }
+            
+            menuService.deleteMenu(menu.getItem_id());
+            
+            tableView.getItems().remove(menu);
+            clearFields();
+        }catch(RemoteException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onRefresh(ActionEvent event) {
+        try{
+            tableView.getItems().setAll(menuService.getAllMenu());
+        }catch(RemoteException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void setMain(Main main)
+    {
+        this.main = main;
+        this.menuService = main.getMenuService();
+        
+        try {
+            tableView.getItems().setAll(menuService.getAllMenu());
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void clearFields() {
+        
+        txtid.setText("");
+        txtname.setText("");
+        txttype.setText("");
+        txtprice.setText(""); 
+    }
+
+    private boolean isFieldValid() {
+        
+        String errorMessage = "";
+        
+        if(txtname.getText()==null || txtname.getText().isEmpty())
+        {
+            errorMessage += "No item entered\n";
+        }
+        
+        if(txttype.getText() == null || txttype.getText().isEmpty())
+        {
+            errorMessage += "Type not mentioned\n";
+            
+        }
+        
+        if(txtprice.getText()== null || txtprice.getText().isEmpty())
+        {
+            errorMessage += "Price not entered";
+        }
+        
+        if(errorMessage.length()==0)
+        {
+            return true;
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initModality(Modality.APPLICATION_MODAL);
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Enter all the fields");
+            alert.setContentText(errorMessage);
+            alert.showAndWait();
+            return false;
+        }
+    }
+}
---
+++ rmi-database-client/src/com/dt/projects/database/client/Main.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dt.projects.database.client;
+
+import com.dt.projects.database.api.entity.Menu;
+import com.dt.projects.database.api.services.MenuService;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author kastu
+ */
+public class Main extends Application{
+
+    private MenuService menuService;
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Registry registry = LocateRegistry.getRegistry("localhost", 52360);
+        
+        menuService = (MenuService) registry.lookup("service");
+        
+        /*Menu m = new Menu();
+         m.setItem_name("Chicken Biryani boneless");
+         m.setItem_type("Non-Veg");
+         m.setItem_price(225);
+         
+         m = menuService.insertMenu(m);
+         
+         System.out.println(m.getItem_Id());
+         System.out.println(m.getItem_name());*/
+        
+        
+        /*FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        loader.setController(new MainController(path));
+        Pane mainPane = loader.load();*/
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("form.fxml"));
+        //loader.setController(new FormController()); 
+        
+        Parent root = loader.load();
+        
+        FormController controller = loader.getController();
+        
+        controller.setMain(this);
+        
+        stage.setScene(new Scene(root));
+        
+        stage.setTitle("Menu");
+        stage.show();
+        
+    }
+    
+    public MenuService getMenuService()
+    {
+        return menuService;
+    }
+    
+    public static void main(String[] args) {
+        
+        launch(args);
+    }
+
+       
+}
